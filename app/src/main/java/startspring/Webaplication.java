d0d27e076f3dcf727ffd1119def4cfd6349aba41
gustavo21332 <pindamom54@gmail.com>
2023-05-05 15:14:29 +0000

    salvar

---
+++ app/src/main/java/startspring/Webaplication.java
@@ -0,0 +1,11 @@
+package startspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webaplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Webaplication.class, args);
+    }
+}
